2bc4c41b58a9980657b1faf2d6bd0cd3979c5fbc
Kunal Sinha <kusinha@microsoft.com>
2017-05-03 02:08:52 -0400

    Fixed grid headers to headers rather than buttons and added sortable functionality on click

--- src/assets/scripts/addEntry.js
+++ src/assets/scripts/addEntry.js
@@ -63,3 +63,55 @@ $('document').ready(function(){
         
     });
 });
+
+/* Function to make table headers sortable */
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable2");
+  switching = true;
+  dir = "asc"; 
+  /*While still unsorted */
+  while (switching) {
+    switching = false;
+    /*save all the rows in table*/
+    rows = table.getElementsByClassName("sortme");
+    for (i = 0; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*save items in the i'th column*/
+      x = rows[i].getElementsByClassName("sortablerow")[n];
+      y = rows[i + 1].getElementsByClassName("sortablerow")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
