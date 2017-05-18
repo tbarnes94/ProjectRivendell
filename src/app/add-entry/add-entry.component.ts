436087bc025c2a4b8b163480654683bd98f2cfbd
Kevin Guo <kegu@microsoft.com>
2017-05-18 17:45:36 -0500

    Added an add-entry view that works for every field except for forecasts

--- src/app/add-entry/add-entry.component.ts
+++ src/app/add-entry/add-entry.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Entity } from 'app/Entity';
+import { DataService } from 'app/data.service'
 
 @Component({
   selector: 'app-add-entry',
-  templateUrl: './add-entry.component.html',
+  templateUrl: './add-entry.componenttwo.html',
   styleUrls: ['./add-entry.component.css']
 })
-export class AddEntryComponent implements OnInit {
+export class AddEntryComponent{
   tiles = [
     {text: 'One', cols: 2, rows: 2, color: 'lightblue'},
     {text: 'Two', cols: 2, rows: 2, color: 'lightgreen'}
   ];
 
-  constructor() { }
+  likelihoods = ["Low", "Medium", "High"];
+  intext = ["Internal", "External"];
+  model = new Entity();
 
-  ngOnInit() {
+  constructor(private _dataService: DataService) {
   }
+
+  onSubmit(){
+    this._dataService.postService(this.model)
+    .subscribe(result => {
+      console.log(JSON.stringify(result));
+    });
+  }
+
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
